12a5452608be9d8c62d4fa01d2e12c2793cf2e3e
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-05-29 10:54:53 -0400

    Régler warning dans la console Ajouter key pour le map, enlever tag body :)

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -4,20 +4,23 @@ import { NavLink } from "react-router-dom";
 
 function Accueil() {
   return (
-    <body>
+  
       <div className="banner">
         <img src={`img/banner.png`} alt="Banner Image" />
         <div className="content">
           <h1> Films illimités </h1>
-          {json.map((text) => (
-            <p className="para">{text}</p>
+          {json.map((text, index) => (
+            <p className="para" key={index}>
+              {text}
+            </p>
           ))}
+
           <div className="button-accueil">
             <NavLink to="Films">Films</NavLink>
           </div>
         </div>
       </div>
-    </body>
+ 
   );
 }
 
